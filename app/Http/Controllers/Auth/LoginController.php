9352507dd92218074b63bbb5a0fc5518c34c66b5
naorone <naorone@gmail.com>
2017-10-20 13:52:50 +0200

    username or email

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -37,8 +38,18 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+    protected function credentials(Request $request)
+    {
+        $field = filter_var($request->input($this->username()), FILTER_VALIDATE_EMAIL) ? 'email' : 'name';
+        $request->merge([$field => $request->input($this->username())]);
+        return $request->only($field, 'password');
+    }
+
     public function username()
     {
-        return 'name';
+
+        return 'email';
+
     }
+
 }
